8f16ecc8ebffc4d11b2ba0b492a5407be836fd77
Joever E Monceda <joever.monceda@gmail.com>
2020-10-15 22:34:21 +0700

    Init Project

---
+++ JSanitizer.Tests/Controllers/JSanitizerController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JSanitizer.Tests.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JSanitizerController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<JSanitizerController> _logger;
+
+        private string _xmlValue { set; get; }
+
+        public JSanitizerController(ILogger<JSanitizerController> logger)
+        {
+            _logger = logger;
+
+            string path = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "_test.xml");
+
+            _xmlValue = System.IO.File.ReadAllText(path);
+        }
+
+        [HttpGet, Route("GetWithoutOptions")]
+        public IActionResult Get()
+        {
+          
+            Data data = new Data()
+            {
+                JsonArrayResult = "[{\"password\":\"password@412\", \"data\": \"sample\" }]".SanitizeJsonValue(),
+                Name = "{\"name\":\"John\", \"age\":30, \"password\":\"password@123\" } ]}".SanitizeJsonValue(),
+                XMLResult = _xmlValue.SanitizeXmlValue()
+            };
+
+            return Ok(data);
+        }
+
+        [HttpGet, Route("GetWithOptions")]
+        public IActionResult GetWithOptions()
+        {
+            Data data = new Data()
+            {
+                XMLResult = _xmlValue.SanitizeXmlWithOptions(new Sanitizer.JOptions()
+                {
+                    DefaultMaskValue = "####-####",
+                    Sensitivity = new List<string>() { "password" }
+                }),
+            };
+
+            return Ok(data);
+        }
+
+        public class Data
+        {
+            public string IdCard { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string XMLResult { set; get; }
+            public string JsonResult { set; get; }
+            public string JsonArrayResult { set; get; }
+        }
+    }
+}
---
+++ JSanitizer/Sanitizer.cs
@@ -0,0 +1,295 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace JSanitizer
+{
+    public static class Sanitizer
+    {
+        private static void XmlValueReplacer(XmlNode node, string sensitivity, string maskVal)
+        {
+            foreach (XmlNode child in node.ChildNodes)
+            {
+                if (child.SelectSingleNode(sensitivity) != null)
+                {
+                    child.SelectSingleNode(sensitivity).InnerText = maskVal;
+                }
+            }
+        }
+
+        private static LogConfig GetConfig()
+        {
+            string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "sanitizerOptions.json");
+
+            string jsonValue = File.ReadAllText(filePath);
+
+            return JsonConvert.DeserializeObject<LogConfig>(jsonValue);
+        }
+
+        public class JOptions
+        {
+            public JOptions ()
+            {
+                this.Sensitivity = new List<string>();
+            }
+
+            public string DefaultMaskValue { set; get; } = "###-###";
+            public List<string> Sensitivity { set; get; }
+        }
+
+        public static string SanitizeXmlWithOptions(this string target, JOptions options)
+        {
+
+            if (string.IsNullOrEmpty(target))
+                return null;
+
+            if (!target.IsValidXML())
+                return target;
+
+            XmlDocument targetXMLDoc = new XmlDocument();
+
+            targetXMLDoc.LoadXml(target);
+
+            if (options.Sensitivity.Count == 0)
+            {
+                options.Sensitivity = new List<string>()
+                {
+                    "password"
+                };
+            }
+
+
+            foreach (string sensitiveItem in options.Sensitivity)
+            {
+                XmlNodeList nodes = targetXMLDoc.DocumentElement.ChildNodes;
+
+                foreach (XmlNode node in nodes)
+                {
+                    XmlValueReplacer(node, sensitiveItem, options.DefaultMaskValue);
+                }
+            }
+
+            return targetXMLDoc.InnerXml;
+        }
+
+        public static string SanitizeXmlValue(this string target)
+        {
+
+            if (string.IsNullOrEmpty(target))
+                return null;
+
+            if (!target.IsValidXML())
+                return target;
+
+            LogConfig logConfig = GetConfig();
+
+            XmlDocument targetXMLDoc = new XmlDocument();
+
+            targetXMLDoc.LoadXml(target);
+
+            logConfig.ConfigurationValue.ForEach(extLog =>
+            {
+                extLog.XmlMask.Sensitivity.ForEach(sensitiveItem =>
+                {
+                    XmlNodeList nodes = targetXMLDoc.DocumentElement.ChildNodes;
+
+                    foreach (XmlNode node in nodes)
+                    {
+                        XmlValueReplacer(node, sensitiveItem, extLog.XmlMask.MaskValue);
+                    }
+                });
+            });
+
+            return targetXMLDoc.InnerXml;
+        }
+
+
+        public static string SanitizeJsonWithOptions(this string target, JOptions options)
+        {
+
+            if (string.IsNullOrEmpty(target)) return null;
+
+            if (!target.IsValidJson())
+                return target;
+
+            if(options.Sensitivity.Count == 0)
+            {
+                options.Sensitivity = new List<string>()
+                {
+                    "password"
+                };
+            }
+
+            foreach (string sensitiveValue in options.Sensitivity)
+            {
+                target = JsonAction(target, sensitiveValue, options.DefaultMaskValue);
+            }
+
+            return target;
+        }
+
+        public static string SanitizeJsonValue(this string target)
+        {
+
+            if (string.IsNullOrEmpty(target)) return null;
+
+            if (!target.IsValidJson())
+                return target;
+
+            LogConfig logConfig = GetConfig();
+
+            logConfig.ConfigurationValue.ForEach(extLog =>
+            {
+                extLog.JsonMask.Sensitivity.ForEach(sensitiveValue =>
+                {
+                    target = JsonAction(target, sensitiveValue, extLog.JsonMask.MaskValue);
+                });
+
+            });
+
+            return target;
+        }
+
+        private static string JsonAction(string propVal, string senItem, string maskValue)
+        {
+            JToken node = JToken.Parse(propVal);
+
+            CheckJsonNode(node, n =>
+            {
+                JToken token = n[senItem];
+
+                if (token != null && token.Type == JTokenType.String)
+                {
+                    propVal = propVal.Replace(token.ToString(), maskValue);
+                }
+            });
+
+            return propVal;
+        }
+
+
+        private static void CheckJsonNode(JToken node, Action<JObject> action)
+        {
+            if (node.Type == JTokenType.Object)
+            {
+                action((JObject)node);
+
+                foreach (JProperty child in node.Children<JProperty>())
+                {
+                    CheckJsonNode(child.Value, action);
+                }
+            }
+            else if (node.Type == JTokenType.Array)
+            {
+                foreach (JToken child in node.Children())
+                {
+                    CheckJsonNode(child, action);
+                }
+            }
+        }
+
+        public static bool IsValidXML(this string xml)
+        {
+            if (!string.IsNullOrEmpty(xml) && xml.TrimStart().StartsWith("<"))
+            {
+                try
+                {
+                    XDocument.Parse(xml);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            else return false;
+        }
+
+        public static bool IsValidJson(this string json)
+        {
+            if (string.IsNullOrEmpty(json)) { return false; }
+
+            json = json.Trim();
+
+            if ((json.StartsWith("{") && json.EndsWith("}")) ||
+                (json.StartsWith("[") && json.EndsWith("]")))
+            {
+                try
+                {
+                    JToken.Parse(json);
+
+                    return true;
+                }
+                catch (JsonReaderException)
+                {
+                    return false;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+    }
+
+    public class LogConfig
+    {
+        public LogConfig()
+        {
+            this.ConfigurationValue = new List<ConfigurationValue>();
+        }
+
+        public string DefaultMaskValue { set; get; }
+        public List<ConfigurationValue> ConfigurationValue { set; get; }
+    }
+
+    public class ConfigurationValue
+    {
+        public ConfigurationValue()
+        {
+            this.XmlMask = new XmlMask();
+            this.JsonMask = new JsonMask();
+        }
+
+        public int Id { set; get; }
+        public string ServiceName { set; get; }
+
+        public XmlMask XmlMask { set; get; }
+
+        public JsonMask JsonMask { set; get; }
+    }
+
+    public class XmlMask
+    {
+        public XmlMask()
+        {
+            this.Sensitivity = new List<string>();
+        }
+
+        public string MaskValue { set; get; }
+        public List<string> Sensitivity { set; get; }
+    }
+
+    public class JsonMask
+    {
+        public JsonMask()
+        {
+            this.Sensitivity = new List<string>();
+        }
+
+        public string MaskValue { set; get; }
+        public List<string> Sensitivity { set; get; }
+    }
+}
